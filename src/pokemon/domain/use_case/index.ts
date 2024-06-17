17675165cb39fee075a3e229147199c1170e62bb
dev002 <dev002@Mac-mini-de-datacasasadmin.local>
2024-06-17 16:26:19 +0200

    delete

--- src/pokemon/domain/use_case/index.ts
+++
@@ -1,6 +0,0 @@
-
-export * from './get_pokemons/GetPokemonsUseCase';
-export * from './get_pokemon/GetPokemonUseCase';
-export * from './get_pokemons_with_name_id/GetPokemonsWithNameIdUseCase';
-export * from './get_pokemons_by_id/GetPokemonsByIdsUseCase';
-
